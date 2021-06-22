9230cb3b9e2ec2991478a8fc5e469952cc39b18a
luizsp7m <62671884+luizsp7m@users.noreply.github.com>
2021-06-22 19:21:50 -0300

    Aula 02

---
+++ src/pages/Home.tsx
@@ -0,0 +1,58 @@
+import illustrationImg from '../assets/illustration.svg';
+import logoImg from '../assets/logo.svg';
+import googleIconImg from '../assets/google-icon.svg';
+
+import '../styles/auth.scss';
+
+import Button from '../components/Button';
+
+import { useHistory } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+function Home() {
+  const history = useHistory();
+
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="#" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire dúvidas de sua audiência em tempo real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="#" />
+
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="#" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
